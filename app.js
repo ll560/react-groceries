1f8b20db8284d8799cc87fb0b0b389f982ed9181
ll560 <llavia6@gmail.com>
2022-06-05 21:32:22 -0400

    added css

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 console.log('Hello World')
 
+
 class App extends React.Component {
+    
     state = {
-        products: '',
+        products: [''],
         item: '',
         brand: '',
+        units:'',
         quantity: 0,
-        isPurchased: false
+        isPurchased: true
     }
 
 handleChange = (event) => {
@@ -23,16 +26,19 @@ handleSubmit = (event) => {
     const newItem = {
         item: this.state.item,
         brand: this.state.brand,
+        units: this.state.units,
         quantity: this.state.quantity,
 
     }
+    console.log(newItem)
 
     this.setState({
         products: [newItem, ...this.state.products],
         item: '',
         brand: '',
-        quantity: 0
-
+        units: '',
+        quantity: 0,
+        isPurchased: true
     })
 }
 
@@ -41,25 +47,40 @@ render(){
     return(
         <div>
             <h1>Grocery List</h1>
-
+            <div className="top">
             <form onSubmit={this.handleSubmit}>
+                <p>Submit items to the list:</p>
                 <label htmlFor="item">Item:</label>
-                <input type="text" id="item" value={this.state.item} onChange={this.handleChange}/>
+                <input type="text" id="item" value={this.state.item} onChange={this.handleChange} onClick={this.toogleHiring}/>
                 <label htmlFor="brand">Brand:</label>
                 <input type="text" id="brand" value={this.state.brand} onChange={this.handleChange}/>
+                <label htmlFor="units">Units:</label>
+                <input type="text" id="units" value={this.state.units} onChange={this.handleChange}/>
                 <label htmlFor="quantity">Quantity:</label>
                 <input type="number" id="quantity" value={this.state.quantity} onChange={this.handleChange}/>
+
                 <input type="submit"></input>
             </form>
-
-            <div>
+            </div>
+            <div className="top">
                 <h2>Preview New Item</h2>
                 <h3>Item: {this.state.item}</h3>
                 <h4>Brand: {this.state.brand}</h4>
+                <h4>Units: {this.state.units}</h4>
                 <h5>Quantity: {this.state.quantity}</h5>
             </div>
 
+            <div className="top">
+            <h2>Grocery Items</h2>
+            {this.state.isPurchased ? <div><ul>
+                {this.state.products.map(product => 
+                    <li>{product.item} &emsp; {product.brand} &emsp; {product.units} &emsp; {product.quantity} </li>
+                
+                )} 
+            </ul> </div> : "We did not buy any items yet"}
             
+
+            </div>
             
         </div>
 
